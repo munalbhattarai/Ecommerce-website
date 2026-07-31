c15e6f53e72747a07590acaf401a4fe851df0f13
munal <emailaddress9810@gmail.com>
2026-07-31 14:59:02 +0545

    Register and Profile serializer

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -12,3 +12,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username" , "email"]
