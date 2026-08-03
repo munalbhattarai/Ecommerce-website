6cf2fb7ccfa3aaf0e4790a7304d7d62ec92a450a
munal <emailaddress9810@gmail.com>
2026-08-03 21:14:45 +0545

    filtering and urls

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import CategoryListCreateView , ProductListCreateView , ProductRetriveUpdateDestroy
+from .views import CategoryListCreateView , ProductListCreateView , ProductRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name = 'category-list'),
   
     path('products/', ProductListCreateView.as_view(), name= "product-list"),
     
-    path('products/<int:pk>/', ProductRetriveUpdateDestroy.as_view() , name ="product"),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view() , name ="product"),
     
 ]
--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
-from rest_framework import filters
 
-# Create your views here.
 
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    
+
+
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    filter_backends = [filters.SearchFilter]
-    search_fields = ["name", "brand", "model"]
-    
-class ProductRetriveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    search_fields = ["name", "brand"]
+    ordering_fields = ["price", "created_at", "stock"]
+    ordering = ["-created_at"]
+
+
+class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    
