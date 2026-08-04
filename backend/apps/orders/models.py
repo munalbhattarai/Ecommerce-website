aefa365c7b0a58cc08bd14106cbb6ea15090dfb1
munal <emailaddress9810@gmail.com>
2026-08-04 14:06:44 +0545

    models for orders

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Orders(models.Model):
+    STATUS_CHOICES = [
+        ("PENDING","Pending"),
+        ("PROCESSING", "Processing"),
+        ("SHIPPED", "Shipped"),
+        ("DELIVERED", "Delivered"),
+        ("CANELLED", "Cancelled"),
+    ]
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+    shipping_address = models.TextField()
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="PENDING"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"order #{self.id}"
