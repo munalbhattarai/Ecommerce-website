8de3c6ccaec0277cecc7f72e6539266490eee88c
munal <emailaddress9810@gmail.com>
2026-08-04 14:54:08 +0545

    fixes

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Orders(models.Model):
+class Order(models.Model):
     STATUS_CHOICES = [
         ("PENDING","Pending"),
         ("PROCESSING", "Processing"),
