6aef2f1fe25e35ae7d5e303e9373d223bced4c34
munal <emailaddress9810@gmail.com>
2026-08-04 07:23:01 +0545

    added url for cartview

--- backend/apps/cart/urls.py
+++ backend/apps/cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddToCartView
+from .views import AddToCartView, CartView
 
 urlpatterns = [
     path('add/', AddToCartView.as_view() ,name='add-to-cart'),
+    path('', CartView.as_view(), name="cart")
 ]
