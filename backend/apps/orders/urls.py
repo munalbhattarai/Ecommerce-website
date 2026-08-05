32265f5dbc2263ba5b25c4a435d3ad200f1d0d7e
munal <emailaddress9810@gmail.com>
2026-08-05 11:42:22 +0545

    orderdetails view url

--- backend/apps/orders/urls.py
+++ backend/apps/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderListView, PlaceOrderView
+from .views import OrderListView, PlaceOrderView, OrderDetailView
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='order-list'),
     path('place/', PlaceOrderView.as_view(), name="place-order"),
+    path('<int:pk>/', OrderDetailView.as_view(), name="order-details"),
 ]
