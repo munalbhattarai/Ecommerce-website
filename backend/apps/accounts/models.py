3b80b9a5ff55a1c4154d8cc5a53af37dd19c2e4c
munal <emailaddress9810@gmail.com>
2026-08-05 13:48:39 +0545

    profile models for selecting the roles

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,4 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+
+    BUYER = "BUYER"
+    SELLER = "SELLER"
+    ADMIN = "ADMIN"
+
+    ROLE_CHOICES = [
+        (BUYER, "Buyer"),
+        (SELLER, "Seller"),
+        (ADMIN, "Admin"),
+    ]
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=BUYER
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
