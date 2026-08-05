3f0d855cf3b75b332671b0b8f1dee9869da27e34
munal <emailaddress9810@gmail.com>
2026-08-05 11:43:09 +0545

    order items model

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from apps.products.models import Product
 
 # Create your models here.
 class Order(models.Model):
@@ -31,3 +32,27 @@ class Order(models.Model):
     
     def __str__(self):
         return f"order #{self.id}"
+    
+
+class OrderItem(models.Model):
+    
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT
+    )
+
+    quantity = models.PositiveIntegerField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
