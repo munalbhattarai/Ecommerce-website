fd34c1c480f42c0387db15e3d034330a7084db7b
munal <emailaddress9810@gmail.com>
2026-08-05 09:46:12 +0545

    added all necessary serializer classes

--- backend/apps/orders/serializers.py
+++ backend/apps/orders/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, OrderItem
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,4 +15,29 @@ class OrderListSerializer(serializers.ModelSerializer):
             "status",
             "total_amount",
             "created_at",
+        ]
+        
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField()
+
+    class Meta:
+        model = OrderItem
+        fields = [
+            "product",
+            "quantity",
+            "price",
+        ]
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "status",
+            "shipping_address",
+            "total_amount",
+            "created_at",
+            "items",
         ]
