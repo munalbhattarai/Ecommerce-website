1aeeeb5dd36cbe7dd1f919eebdcd66ba2591fabc
munal <emailaddress9810@gmail.com>
2026-08-05 09:25:18 +0545

    added new order list view

--- backend/apps/orders/views.py
+++ backend/apps/orders/views.py
@@ -3,10 +3,12 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .serializers import OrderSerializer
 from .models import Order, OrderItem
 
+from .serializers import OrderListSerializer
 
 class PlaceOrderView(APIView):
     permission_classes = [IsAuthenticated]
@@ -73,3 +75,13 @@ class PlaceOrderView(APIView):
             },
             status=status.HTTP_201_CREATED,
         )
+        
+
+class OrderListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrderListSerializer
+    
+    def get_queryset(self):
+        return Order.objects.filter(
+            user = self.request.user
+        ).order_by("-created_at")
