ac8e6da7af04fd4154e6f220732f478710387dc6
munal <emailaddress9810@gmail.com>
2026-08-06 07:05:32 +0545

    added seller dashboard url in orders url

--- backend/apps/orders/urls.py
+++ backend/apps/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import OrderListView, PlaceOrderView, OrderDetailView, CancelOrderView
+from .views import OrderListView, PlaceOrderView, OrderDetailView, CancelOrderView, SellerDashboardView
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='order-list'),
     path('place/', PlaceOrderView.as_view(), name="place-order"),
     path('<int:pk>/', OrderDetailView.as_view(), name="order-details"),
     path("<int:pk>/cancel/", CancelOrderView.as_view(), name="cancel-order"),
+    path("seller/dashboard/", SellerDashboardView.as_view(), name="seller-dashboard" ),
 ]
