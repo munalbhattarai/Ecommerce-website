d451159ecc2c239d27059fc79c3d1f1bb50879cf
munal <emailaddress9810@gmail.com>
2026-08-09 08:06:23 +0545

    Register validation

--- frontend/src/features/auth/validation/authSchema.js
+++ frontend/src/features/auth/validation/authSchema.js
@@ -9,3 +9,30 @@ export const loginSchema = z.object({
         .string()
         .min(1, "Password is required"),
 });
+
+
+export const registerSchema = z.object({
+
+    username: z
+        .string()
+        .min(3, "Username must be at least 3 characters"),
+
+    email: z
+        .string()
+        .email("Enter a valid email"),
+
+    password: z
+        .string()
+        .min(8, "Password must be at least 8 characters"),
+
+    password2: z
+        .string()
+        .min(8, "Please confirm your password"),
+
+}).refine(
+    (data) => data.password === data.password2,
+    {
+        message: "Passwords do not match",
+        path: ["password2"],
+    }
+);
