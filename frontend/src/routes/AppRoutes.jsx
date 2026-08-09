22bf727f1868870df48353b21c680e7b831b324d
munal <emailaddress9810@gmail.com>
2026-08-09 08:30:42 +0545

    Created protected routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,22 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
+import ProtectedRoute from "../features/auth/components/ProtectedRoutes";
 
 
 function Home() {
     return <h1>Home</h1>;
+}
+function Cart() {
+    return <h1>Cart</h1>;
+}
+
+function Orders() {
+    return <h1>Orders</h1>;
 }
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
+                <Route path="/" element={<Home />}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
+
+                <Route element={<ProtectedRoute />}>
+
+                    <Route path="/cart" element={<Cart />}/>
+                    <Route path="/orders" element={<Orders />}/>
+
+                </Route>
+
             </Routes>
         </BrowserRouter>
     );
